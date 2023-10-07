3895527ff0f2cdb934bb93f307f7bd7267905dd7
Megan M <megankmithen@gmail.com>
2023-10-08 10:07:40 +1100

    added lie logic, confetti, styles

--- js/main.js
+++ js/main.js
@@ -40,7 +40,8 @@ if(state.board[2] === playersTurn && state.board[4] === playersTurn && state.boa
 const state = {
     board: null,
     turn: null,
-    winner: null
+    winner: "playing",
+    turnCount: 0
 }
 
 /*----- cached elements  -----*/
@@ -67,7 +68,8 @@ init();
 function init(){
     state.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     state.turn = 1;
-    state.winner = null;
+    state.winner = "playing";
+    state.turnCount = 0;
     document.getElementById("board").addEventListener('click', handleClick);
     render();
 }
@@ -81,15 +83,21 @@ function handleClick(clickEvent){
         if (clickedSquare === value){
             //check if item in state.board is equal to 0. If it is, update it to current player's value. If not, do nothing.
             if(state.board[index] === 0){
-                let turnCount = 
+                state.turnCount += 1;
+                console.log(state.turnCount);
                 state.board[index] = state.turn;
                 let result = checkWinner(state.turn);
                 if(result !== 0 && result !== undefined){
-                    state.winner = true;
+                    state.winner = "winnerFound";
                     console.log("winner is: " + COLOURS[state.turn]);
                     render();
                     document.getElementById("board").removeEventListener("click", handleClick);
                     return;
+                }
+
+                if(state.turnCount === 9){
+                    state.winner = "tieDetected";
+                    render();
                 }
                 state.turn *= -1;
             }
@@ -162,27 +170,38 @@ function render() {
 }
 
 function renderMessage(){
-    if (state.winner) {
-        elements.message.innerHTML = `<span style="color: ${ SYMBOLS[state.turn] }">${ SYMBOLS[state.turn] } wins!</span>`;  
-        // <img height=180px src='assets/playerX.png'> 
+    if (state.winner === "winnerFound") {
+        elements.message.innerHTML = `<span style="font-size: 5vmin">${ SYMBOLS[state.turn] } WINS!</span>`;  
+        start();
+        stop();
+    }
+    else if (state.winner === "tieDetected"){
+        elements.message.innerText = "It's a tie \n ¯\\_(ツ)_/¯"
     }
     else {
-        elements.message.innerHTML = `<span style="color: ${ SYMBOLS[state.turn] }">${ SYMBOLS[state.turn] }'s turn</span>`;
+        elements.message.innerHTML = `<span>${ SYMBOLS[state.turn] }'s turn</span>`;
     }
-    // TODO: show tie
+
     
 }
 
 
-//   5.2) If the board has a value at the index, immediately return because that square is already taken.
-//   5.3) If winner is not null, immediately return because the game is over.
-//   5.4) Update the board array at the index with the value of turn.
-//   5.5) Flip turns by multiplying turn by -1 (flips a 1 to -1, and vice-versa).
-//   5.6) Set the winner variable if there's a winner:
-//     5.6.1) Loop through the each of the winning combination arrays defined.
-//     5.6.2) Total up the three board positions using the three indexes in the current combo.
-//     5.6.3) Convert the total to an absolute value (convert any negative total to positive).
-//     5.6.4) If the total equals 3, we have a winner! Set winner to the board's value at the index specified by the first index in the combo array. Exit the loop.
-//   5.7) If there's no winner, check if there's a tie:
-//     5.7.1) Set winner to 'T' if there are no more nulls in the board array.
-//   5.8) All state has been updated, so render the state to the page (step 4.2).
+// ---- PLEASE NOTE, I DID NOT WRITE THE BELOW CODE ----//
+
+// Source for instructions: https://dev.to/official_fire/creating-a-confetti-effect-in-5-minutes-16h3
+// Soure for confetti.js file: https://github.com/CoderZ90/confetti/blob/main/confetti.js
+
+// for starting the confetti 
+const start = () => {
+    setTimeout(function() {
+        confetti.start()
+    }, 0.1000); 
+}
+//  for stopping the confetti 
+const stop = () => {
+    setTimeout(function() {
+        confetti.stop()
+    }, 2000); 
+}
+
+// ---------------------------------------------------- //
